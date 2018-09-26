75e313628de32ea8c6286387d9014a07308132d6
davidatkooltra <david.jorjani@kooltra.com>
2018-09-26 18:37:05 -0400

    Took out commented code from Main clas

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
     Person Mike = new Person("Mike", "Jordan", "456");
     Person Mike2 = new Person("Mike", "Jordan", "456");
     String[] people = new String[] {"Joe"};
-    // System.out.println(Mike2.toString());
     System.out.println(Mike.equals(Mike2));
   }
 }
