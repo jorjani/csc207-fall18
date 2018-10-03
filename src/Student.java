30ff2726c997ccb0b00b92e2f74cfdae3e6441a5
davidatkooltra <david.jorjani@kooltra.com>
2018-10-03 17:23:37 -0400

    Merge branch 'master' of github.com:jorjani/csc207-fall18
    # Conflicts:
    #	src/Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,9 +1,23 @@
-public class Student extends Person {
+public class Student extends Person implements SystemUser {
   int studentNumber;
   String[] courses;
   double gpa = 4;
+
+  public Student(String firstName, String lastName, String SIN) {
+    this.SIN = SIN;
+    this.name = new String[] {firstName, lastName};
+  }
+
+  @Override
+  public void goToLecture() {
+    System.out.println("Damn! Again?!");
+  }
+
   @Override
   public String toString() {
     return "Student{" + "studentNumber=" + studentNumber + '}';
   }
+
+  @Override
+  public void checkBalance() {}
 }
